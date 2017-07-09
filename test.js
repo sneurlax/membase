167d7d28fb3406ab182aa25f6f822c0156b3f9cd
sneurlax <sneurlax@gmail.com>
2017-07-09 00:15:20 -0500

    proof of concept returning raw hex of tx in mempool
    need to decode.

--- test.js
+++ test.js
@@ -1,14 +1,38 @@
+// JSON-RPC
 const rpcAuth = require('./rpcAuth.js');
-const Client = require('bitcoin-core');
-const client = new Client({ username: rpcAuth.rpcuser, password: rpcAuth.rpcpassword});
-
+var bitcoinRpc = require('./node_modules/bitcoin')
+var rpcClient = new bitcoinRpc.Client({
+  host: 'localhost',
+  port: 8332,
+  user: rpcAuth.rpcuser,
+  pass: rpcAuth.rpcpassword,
+  timeout: 30000
+});
 // client.getInfo().then((help) => console.log(help));
 
+// Firebase
 var admin = require('firebase-admin');
-
-var serviceAccount = require('serviceAccountKey.json');
-
+var serviceAccount = require('./serviceAccountKey.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://bitphilia-f8be0.firebaseio.com'
 });
+
+var monitor = function() {
+  // rpcClient.getBalance('*', 6, function(err, balance, resHeaders) {
+  //   if (err) return console.log(err);
+  //   console.log('Balance:', balance);
+  // });
+  rpcClient.getRawMemPool(function(err, memPool) {
+    if(err) return console.log(err);
+    for( tx in memPool ) {
+      console.log('tx '+tx+': '+memPool[tx]);
+      rpcClient.getRawTransaction(memPool[tx], function(err, txInfo) {
+        if(err) return console.log(err);
+        console.log(txInfo);
+      });
+    }
+  });
+}
+
+setInterval(monitor, 1000);
