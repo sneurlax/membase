dd82117c062fd52fad24b60dc9198f0d8e424e57
sneurlax <sneurlax@gmail.com>
2017-07-08 23:28:27 -0500

    init stub

--- test.js
+++ test.js
@@ -2,4 +2,13 @@ const rpcAuth = require('./rpcAuth.js');
 const Client = require('bitcoin-core');
 const client = new Client({ username: rpcAuth.rpcuser, password: rpcAuth.rpcpassword});
 
-client.getInfo().then((help) => console.log(help));
+// client.getInfo().then((help) => console.log(help));
+
+var admin = require('firebase-admin');
+
+var serviceAccount = require('serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://sample-project.firebaseio.com'
+});
