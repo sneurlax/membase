941f7f64cb4d0e943ccc56ea7dbd54550ba1d5da
sneurlax <sneurlax@gmail.com>
2017-07-17 17:50:35 -0500

    reverting this HORSESHIT

--- test.js
+++ test.js
@@ -1,18 +1,4 @@
 // JSON-RPC
-const rpcAuth = require('./rpcAuth.js');
-var bitcoinRpc = require('./node_modules/bitcoin')
-var rpcClient = new bitcoinRpc.Client({
-  host: 'localhost',
-  port: 8332,
-  user: rpcAuth.rpcuser,
-  pass: rpcAuth.rpcpassword,
-  timeout: 30000
-});
-// rpcClient.getInfo().then((help) => console.log(help));
-rpcClient.getBalance('*', 6, function(err, balance, resHeaders) {
-  if (err) return console.log(err);
-  console.log('Balance:', balance);
-});
 
 // Firebase
 /*
@@ -26,6 +12,7 @@ var db = admin.database();
 var ref = db.ref('mempool');
 */
 
+/*
 var memPool = {}
 
 var monitor = function() {
@@ -37,23 +24,23 @@ var monitor = function() {
         if( err ) return console.log(err);
         // console.log('rawtx: '+rawTx);
         rpcClient.cmd('decoderawtransaction', rawTx, function(err, decodedTx) {
-          if( err ) return /*console.log(err)*/;
+          // if( err ) return console.log(err);
+          if( err ) return console.log(err);
           console.log(decodedTx);
           // memPool[] = decodedTx;
         });
       });
     }
     console.log(memPool);
-    /*
-    for( tx in memPool ) {
-      var memPoolRef = ref.child(tx['hash']);
-      memPoolRef.set({
-        tx
-      });
-    }
-    */
+    // for( tx in memPool ) {
+    //   var memPoolRef = ref.child(tx['hash']);
+    //   memPoolRef.set({
+    //     tx
+    //   });
+    // }
   });
 }
 
 monitor();
 // setInterval(monitor, 1000*60);
+*/
